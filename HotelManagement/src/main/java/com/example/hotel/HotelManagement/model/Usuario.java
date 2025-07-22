d5480fc5846ec41c6fc95f27c83060506239a545
Hugo Denardi Domiciano <hugodenardi@gmail.com>
2025-07-21 21:50:28 -0300

    feat: user implement

---
+++ HotelManagement/src/main/java/com/example/hotel/HotelManagement/model/Usuario.java
@@ -0,0 +1,10 @@
+package com.example.hotel.HotelManagement.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Usuario {
+    private String usuario;
+    private String senha;
+
+}
