4d630943282a07e094b30623f7d6a5f998212b64
Hugo Denardi Domiciano <hugodenardi@gmail.com>
2025-07-23 21:54:40 -0300

    Feat: Login implement

---
+++ HotelManagement/src/main/java/com/example/hotel/HotelManagement/service/TokenService.java
@@ -0,0 +1,46 @@
+package com.example.hotel.HotelManagement.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.example.hotel.HotelManagement.model.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String chaveSecreta;
+
+    public String generateToken(Usuario usuario) {
+        try {
+            Instant tempoExpiracao = LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+            return JWT.create().withIssuer("HotelManagement").withSubject(usuario.getId().toString()).withExpiresAt(tempoExpiracao)
+                    .sign(Algorithm.HMAC256(chaveSecreta));
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Não foi possível gerar o JWT", e);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            return JWT.require(Algorithm.HMAC256(chaveSecreta))
+                    .withIssuer("HotelManagement")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Token inválido", e);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
---
+++ HotelManagement/src/main/java/com/example/hotel/HotelManagement/service/UsuarioService.java
@@ -0,0 +1,23 @@
+package com.example.hotel.HotelManagement.service;
+
+import com.example.hotel.HotelManagement.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService implements UserDetailsService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByUsuario(username).orElseThrow(() ->
+                new RuntimeException("Credenciais não encontradas")
+        );
+    }
+}
